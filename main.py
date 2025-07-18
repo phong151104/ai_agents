622bb37ef6b72f17eac0ae0bc9327fbf3e40a67e
Pham Hung Phong <119754310+phong151104@users.noreply.github.com>
2025-07-18 17:02:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,65 @@
-# This is a sample Python script.
+import logging
+import os
+import sys
+from dotenv import load_dotenv
+load_dotenv()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from app.database.mongo import enterprise_repo
+from app.database.mongo.enterprise_repo import Enterprise
+from app.util import utils
+
+from fastapi import FastAPI, Request
+from app.api import (
+    llm_filter,
+    utils as utils_api,
+    document,
+    google,
+    json,
+    forwarder
+)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def create_app() -> FastAPI:
+    api = FastAPI(
+        title="Multi-Search API",
+        version="1.0.0",
+        description="Provides LLM.py, Semantic, and Full-text Search APIs.",
+    )
+
+    return api
+
+#Test
+api = create_app()
+api.include_router(llm_filter.router, prefix="/api/v1/filter")
+api.include_router(utils_api.router, prefix="/api/v1/utils")
+api.include_router(document.router, prefix="/api/v1/document")
+api.include_router(google.router, prefix="/api/v1/google")
+api.include_router(json.router, prefix="/api/v1/json")
+api.include_router(forwarder.router, prefix="/api/v1/forwarder")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    stream=sys.stdout
+)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@api.middleware("http")
+async def api_log_middleware(request: Request, call_next):
+    start = utils.current_time_ms()
+    method = request.method
+    logger.info(f"[{method.upper()}] {request.url}")
+    response = await call_next(request)
+    response.headers['X-Process-Time'] = str(utils.current_time_ms() - start)
+    return response
+
+if __name__ == "__main__":
+    # enterprise = enterprise_repo.get_enterprise('6502bfaea7cc3b4bcb61c7a6')
+    # print(enterprise)
+    import uvicorn
+
+    uvicorn.run(api, host="0.0.0.0", port=int(os.getenv("PORT", 8080)), reload=False)
+    sys.exit(0)
